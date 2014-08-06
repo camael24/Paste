7b4762f2c41e19a4a88d8eba3b1f718ee4950d0d
thehawk <thehawk@hoa-project.net>
2014-08-06 12:26:27 +0200

    Debug

--- Application/Config/Environnement.php
+++ Application/Config/Environnement.php
@@ -2,6 +2,7 @@
 /**
  * @var \Sohoa\Framework\Environnement $this;
  */
+var_dump(resolove('hoa://Application/Database/base.db'));
 Hoa\Database\Dal::initializeParameters(array(
         'connection.list.default.dal' => Hoa\Database\Dal::PDO,
         'connection.list.default.dsn' => 'sqlite:hoa://Application/Database/base.db',
