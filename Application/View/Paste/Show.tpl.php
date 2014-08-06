1a95e60e8dd965e430abc4dc73540a4c55c96cf5
thehawk <thehawk@hoa-project.net>
2014-08-06 12:57:28 +0200

    php

--- Application/View/Paste/Show.tpl.php
+++ Application/View/Paste/Show.tpl.php
@@ -6,5 +6,5 @@ echo '<p>Title: '.$title.'</p>';
 echo '<p>ID : '.$id.'</p>';
 echo '<p>Url : http://' . \Hoa\Http\Runtime::getHeader('Host') . $router->unroute('show_Paste', array('paste_id' => $id)).'</p>';
 echo '<p>Date : '.date('d/m/Y H:i:s', $time).'</p>';
-echo '<code class="language-php">'.htmlentities($content).'</code>';
+echo '<pre id="p"><code class="language-php">'.htmlentities($content).'</code></pre>';
 $this->endBlock();
